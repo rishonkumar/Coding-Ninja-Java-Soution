8251a9959c2d37d164ac9e1401eae348ab6450e4
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2021-09-01 19:20:27 +0530

    Java DSA

--- MyClassroom/src/Recursion/SearchNumber.java
+++ MyClassroom/src/Recursion/SearchNumber.java
@@ -9,10 +9,19 @@ public class SearchNumber {
         if (input[index] == x) return true;
         return checkNumber2(input,x,index+1);
     }
-
     public static boolean checkNumber(int input[] , int x){
         return checkNumber2(input , x , 0);
     }
+
+    public static boolean checkNumber4(int input[], int start , int end , int x){
+        if(start>end) return false;
+        if(input[start]==x) return true;
+        return checkNumber4(input,start+1, end ,x);
+    }
+    public static boolean checkNumber3(int input[] , int x){
+        return checkNumber4(input ,0 ,input.length-1, x);
+    }
+
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
