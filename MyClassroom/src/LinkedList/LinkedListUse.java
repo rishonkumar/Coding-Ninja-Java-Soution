48ef32e71805879f37a4cf061953cd6697c9c0b2
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2021-09-11 23:50:06 +0530

    Calculated length and Ith Node

--- MyClassroom/src/LinkedList/LinkedListUse.java
+++ MyClassroom/src/LinkedList/LinkedListUse.java
@@ -35,12 +35,43 @@ public class LinkedListUse {
         }
     }
 
+    //To find the length of linked list
+    public static int length(Node<Integer>head){
+        int length=0;
+        Node<Integer> temp = head;
+        while(temp!=null){
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+    //TO find the i th postion in LinkedList
+    public static void printIthNode(Node<Integer>head, int i){
+        if(i>length(head))
+            return;
+        int position = 0;
+        Node<Integer>temp = head;
+        while(temp!=null && position<i){
+            temp=temp.next;
+            position++;
+        }
+        if(temp!=null){
+            System.out.println(temp.data);
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
 
         Node<Integer> head = createLinkedList();
         print(head);
         increment(head);
         print(head);
+        int length = length(head);
+        System.out.println(length);
+        printIthNode(head,2);
 //		Node<Integer> n1 = new Node<>(10); //new node is created
 //		System.out.println(n1); //prints the address of 10
 //		System.out.println(n1.data); // to print the data
