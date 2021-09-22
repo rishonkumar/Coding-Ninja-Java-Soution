711a4b7bd85db5d9003817c3f7ffb9c623a89b7b
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2021-09-22 23:32:20 +0530

    Post Order

---
+++ MyClassroom/src/Trees/Post_Order.java
@@ -0,0 +1,14 @@
+package Trees;
+
+public class Post_Order {
+    public static void postOrder(BinaryTreeNode<Integer> root) {
+
+        if(root == null) return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+
+        System.out.print(root.data);
+
+    }
+}
