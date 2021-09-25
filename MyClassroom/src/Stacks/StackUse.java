e24e2e8b595f69bf260bb0074781b5124d98036f
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2021-09-25 20:00:24 +0530

    Stack Use

--- MyClassroom/src/Stacks/StackUse.java
+++ MyClassroom/src/Stacks/StackUse.java
@@ -5,8 +5,10 @@ public class StackUse {
     public static void main(String[] args) throws StackFullException,StackEmptyException{
 
 //        StackUsingArray stack = new StackUsingArray(100);
-         StackUsingArray stack = new StackUsingArray();
-         int arr[]={5,6,7,1,9};
+//         StackUsingArray stack = new StackUsingArray();
+
+        StackUsingLL<Integer>stack = new StackUsingLL<>();
+        int[] arr ={5,6,7,1,9};
 
         for (int i=0;i<arr.length;i++){
             stack.push(arr[i]);
