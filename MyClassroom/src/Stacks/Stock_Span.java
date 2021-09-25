c79a27637d16a8fd2af3948c4e40ca3a3ff66aa1
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2021-09-25 19:52:39 +0530

    Stock Span using Stack

--- MyClassroom/src/Stacks/Stock_Span.java
+++ MyClassroom/src/Stacks/Stock_Span.java
@@ -1,12 +1,14 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class Stock_Span {
     public static int[] stockSpanBruteForce(int[] price) {
 
         int n = price.length;
         int ans[] = new int[n];
 
-        ans[0]=1; //First value is alaways one
+        ans[0]=1; //First value is always one
 
         for(int i=1; i<n; i++){
             ans[i] = 1;  //initliaze with 1
@@ -17,13 +19,32 @@ public class Stock_Span {
         return ans;
     }
 
-   /* public static int[] stockSpan(int[] price){
+    public static int[] stockSpan(int[] price){
 
-        Stack<Integer>stack = new Stack<>();
+        int n = price.length;
+        int[] spans = new int[n]; //output array
 
-        for(int i=0;i<price.length;i++){
+        Stack<Integer>indexStack = new Stack<>();
 
+        //push the first element in the stack
+        indexStack.push(0);
+        spans[0] = 1; // store index 1 of span as 1 coz it's always 1
+
+        for(int i=1 ;i<n;i++){
+            while(!indexStack.isEmpty() && price[i]>price[indexStack.peek()]){
+                indexStack.pop();
+            }
+
+            if(indexStack.isEmpty()){
+                spans[i] = i+1;
+            }
+            else{
+                spans[i] = i - indexStack.peek();
+            }
+            indexStack.push(i);
         }
-    }*/
+
+        return spans;
+    }
 
 }
