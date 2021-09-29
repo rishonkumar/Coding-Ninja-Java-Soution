3651be2e05e33ca2899a8f16869a4038dea387c4
Rishon Kumar Rahi <rishon.kumar.rahi@gmail.com>
2021-09-29 22:48:22 +0530

    Balanced_Tree Better

--- MyClassroom/src/Trees/Balanced_Tree.java
+++ MyClassroom/src/Trees/Balanced_Tree.java
@@ -27,4 +27,35 @@ public class Balanced_Tree {
 
         return isLeftBalanced && isRightBalanced; //if both satisfy return true
     }
+
+   // TIme complexity - O(n)
+    public static BalancedTreeReturn isBalancedBetter(BinaryTreeNode<Integer>root){
+        if(root == null) {
+            int height = 0;
+            boolean isBal = true;
+            BalancedTreeReturn ans = new BalancedTreeReturn();
+            ans.height = height;
+            ans.isBalanced = isBal;
+            return ans;
+        }
+
+        BalancedTreeReturn leftOutput = isBalancedBetter(root.left);
+        BalancedTreeReturn rightOutput = isBalancedBetter(root.right);
+        boolean isBal = true;
+        int height = 1 + Math.max(leftOutput.height , rightOutput.height);
+
+        if(Math.abs(leftOutput.height - rightOutput.height)>1){
+            isBal = false;
+        }
+
+        if(!leftOutput.isBalanced || !rightOutput.isBalanced){
+            isBal = false;
+        }
+
+        BalancedTreeReturn ans = new BalancedTreeReturn();
+        ans.height = height;
+        ans.isBalanced = isBal;
+        return ans;
+    }
+
 }
